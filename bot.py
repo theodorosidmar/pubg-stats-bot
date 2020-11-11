363bcd2cde02ba3b44f37df7b9b20bae3c1cd645
Sidmar Theodoro <theodorosidmar@gmail.com>
2020-11-10 21:48:30 -0300

    feat: Adds Lifetime cog

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from os import getenv
 from discord.ext import commands
 from pubg_python import PUBG, Shard
+from lifetime import Lifetime
 
 pubg_api = PUBG(getenv('PUBG_API_KEY'), Shard.STEAM)
 
@@ -8,15 +9,9 @@ DISCORD_TOKEN = getenv('DISCORD_TOKEN')
 COMMAND_PREFIX = getenv('DISCORD_COMMAND_PREFIX')
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
+bot.add_cog(Lifetime(bot, pubg_api))
 
-
-@bot.command(name='stats', help='Responds with players stats')
-async def stats(ctx, game_mode, player_name):
-    async with ctx.typing():
-        player = pubg_api.players().filter(player_names=[player_name])[0]
-        stats = pubg_api.seasons('lifetime' , player_id=player.id).get()
-    await ctx.send(stats.attributes['gameModeStats'][game_mode])
-
+# TODO: Need this?
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
