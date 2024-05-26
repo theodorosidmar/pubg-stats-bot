93dc36731752b34f908cb20676d4ee968220b2c8
sid <theodorosidmar@gmail.com>
2024-05-26 20:11:33 -0300

    refactor: Improves Message.toCommand

--- pubg-stats-bot-discord/src/main/kotlin/dev/pubgstats/bot/discord/Message.kt
+++ pubg-stats-bot-discord/src/main/kotlin/dev/pubgstats/bot/discord/Message.kt
@@ -10,16 +10,21 @@ internal fun Message.isValid(): Boolean =
     content.startsWith("!") && !author?.isBot!!
 
 internal fun Message.toCommand(): Command {
-    val (command, arguments) = content.split(' ', limit = 2)
+    val (command, arguments) = content
+        .removePrefix("!")
+        .split(' ')
+        .let {
+            it.first() to it.drop(1)
+        }
     return if (command.lowercase() == "x1") {
-        val (playerOne, playerTwo, gameMode) = arguments.split(' ')
+        val (playerOne, playerTwo, gameMode) = arguments
         X1Command(
             playerOne = playerOne,
             playerTwo = playerTwo,
             gameMode = GameMode.entries.find { it.name.uppercase() == gameMode.uppercase() }!!
         )
     } else {
-        val (player, gameMode) = arguments.split(' ')
+        val (player, gameMode) = arguments
         LifetimeCommand(
             player = player,
             gameMode = GameMode.entries.find { it.name.uppercase() == gameMode.uppercase() }!!
