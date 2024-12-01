834e6a35aa5995ee8dd0f13a18723e1ee46d952a
sid <theodorosidmar@gmail.com>
2024-12-01 20:56:50 -0300

    chore: Uses pubgkt from GitHub packages

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ dependencyResolutionManagement {
     repositories {
         mavenLocal()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/theodorosidmar/pubgkt")
+            credentials {
+                username = "theodorosidmar"
+                password = System.getenv("GITHUB_TOKEN") ?: extra["gpr.key"]?.toString()
+            }
+        }
     }
 }
 
